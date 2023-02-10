7022e01eb9d9fa9d5d54e2356e629011eec36fb0
NikolaiEgorov <n.egorov96@yandex.ru>
2023-02-10 17:37:26 +0300

    lesson1

---
+++ GBShop2/GBShop2App.swift
@@ -0,0 +1,17 @@
+//
+//  GBShop2App.swift
+//  GBShop2
+//
+//  Created by Анастасия Исакова on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct GBShop2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
